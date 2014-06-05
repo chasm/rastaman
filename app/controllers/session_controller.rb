443e1676d228d8fb2130df99f8fc8a4cb917239f
Charles F. Munat <charles.munat@gmail.com>
2014-06-05 11:19:20 +0800

    Added pseudocode for log in.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,15 +5,22 @@ class SessionController < ApplicationController
   end
 
   def create
-    registrant = Registrant.new( registrant_params )
+    # if this is a sign up
+      @registrant = Registrant.new( registrant_params )
 
-    if registrant.save
-      EmailValidator.complete_registration(registrant).deliver
+      if @registrant.save
+        EmailValidator.complete_registration(@registrant).deliver
 
-      render text: "We sent you an email", status: :created
-    else
-      render :new
-    end
+        render text: "We sent you an email", status: :created
+      else
+        render :new
+      end
+
+    # else if this is a log in attempt
+    # find the user with the passed-in email address
+    # and authenticate that user with the passed-in password
+    # if authentication succeeds, set session user_id
+    # and redirect to the root url, otherwise redisplay the form
   end
 
   def destroy
--- app/mailers/email_validator.rb
+++ app/mailers/email_validator.rb
@@ -2,10 +2,9 @@ class EmailValidator < ActionMailer::Base
   default from: 'admin@rastaman.com'
 
   def complete_registration(registrant)
-    @registrant = registrant
-    @url = "http://localhost:3000/register/#{@registrant.sign_up_code}"
+    @url = "http://localhost:3000/register/#{registrant.sign_up_code}"
 
-    mail to: @registrant.email,
+    mail to: registrant.email,
       subject: "Rastaman! Please complete your registration!"
   end
 end
